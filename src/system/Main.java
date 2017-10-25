78f6499079a3cf35384c3ad143ec821ef6434a9a
hydramin <genetamin@gmail.com>
2017-10-25 16:54:41 -0400

    test push

--- src/system/Main.java
+++ src/system/Main.java
@@ -1,6 +1,6 @@
 package system;
 
-public class test{
+public class Main{
     public static void main(String[] args) {
         System.out.println("Hello World!");
         test2 newtest = new test2(10);
