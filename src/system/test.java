6a643660b7b1a5785da6c4726d11891a00aa98a7
hydramin <genetamin@gmail.com>
2017-10-25 16:21:44 -0400

    test commit and test add

---
+++ src/system/test.java
@@ -0,0 +1,9 @@
+package system;
+
+public class test{
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        test2 newtest = new test2(10);
+        System.out.println(newtest.getNum());
+    }
+}
---
+++ src/system/test2.java
@@ -0,0 +1,15 @@
+package system;
+
+public class test2{
+    int num;
+    public test2(int x){
+        this.num = x;
+    }
+
+    /**
+     * @return the num
+     */
+    public int getNum() {
+        return num;
+    }
+}
